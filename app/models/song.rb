850df23595c6e109496f9b8176e0e11ab9ae9caf
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-07 04:50:13 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -24,7 +24,7 @@ end
 def note_contents=(notes)
     notes.each do |content|
       if content.strip != ''
-        self.notes.build(content: content)
+        self.notes.new(content: content)
       end
     end
   end
