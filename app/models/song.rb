e0d43de88eb52dc15d336c76f7277d967b49957d
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-07 04:13:08 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,12 +9,12 @@ def genre_name=(name)
 end
 
 def genre_name
-  self.genre = Genre.find(name: name)
-end
+   self.try(:genre).try(:name)
+ end    
 
-def artist_name
-  self.artist = Artist.find(name: name)
-end
+ def artist_name
+   self.try(:artist).try(:name)
+ end
 
 def artist_name=(name)
   artist = Artist.find_or_create_by(name: name)
